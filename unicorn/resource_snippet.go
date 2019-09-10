0614fd4e327a78644c7e0a218d02a3133f689ca1
Diogenes Jesus <diogenes.jesus@telekom.com>
2019-09-10 23:22:27 +0200

    implement Read, Create, Delete

--- unicorn/resource_snippet.go
+++ unicorn/resource_snippet.go
@@ -1,7 +1,11 @@
 package unicorn
 
 import (
+	"fmt"
+	"strconv"
+
 	"github.com/hashicorp/terraform/helper/schema"
+	gitlab "github.com/xanzy/go-gitlab"
 )
 
 func resourceSnippet() *schema.Resource {
@@ -39,21 +43,71 @@ func resourceSnippet() *schema.Resource {
 }
 
 func resourceSnippetCreate(d *schema.ResourceData, m interface{}) error {
+	client := m.(*gitlab.Client)
+
+	createSnippetOptions := &gitlab.CreateSnippetOptions{
+		Title:       gitlab.String(d.Get("title").(string)),
+		FileName:    gitlab.String(d.Get("file_name").(string)),
+		Content:     gitlab.String(d.Get("content").(string)),
+		Description: gitlab.String("Created by terraform-unicorn"),
+		Visibility:  gitlab.Visibility(gitlab.PublicVisibility),
+	}
+	snippet, _, err := client.Snippets.CreateSnippet(createSnippetOptions)
+
+	if err != nil {
+		return err
+	}
+
+	d.SetId(strconv.Itoa(snippet.ID))
+
 	return resourceSnippetRead(d, m)
 }
 
 func resourceSnippetRead(d *schema.ResourceData, m interface{}) error {
+	client := m.(*gitlab.Client)
+
+	snippetID, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return err
+	}
+
+	snippet, _, err := client.Snippets.GetSnippet(snippetID)
+
+	if err != nil {
+		return err
+	}
+
+	if snippet == nil {
+		fmt.Printf("[WARN] removing snippet id %d from state - not found in gitlab", snippetID)
+		d.SetId("")
+	}
+
 	return nil
 }
 
 func resourceSnippetDelete(d *schema.ResourceData, m interface{}) error {
+	client := m.(*gitlab.Client)
+
+	snippetID, err := strconv.Atoi(d.Id())
+	if err != nil {
+		return err
+	}
+
+	_, err = client.Snippets.DeleteSnippet(snippetID)
+
+	if err != nil {
+		return err
+	}
+
+	d.SetId("")
+
 	return nil
 }
 
-func resourceSnippetUpdate(d *schema.ResourceData, m interface{}) error {
-	return resourceSnippetRead(d, m)
-}
+// func resourceSnippetUpdate(d *schema.ResourceData, m interface{}) error {
+// 	return resourceSnippetRead(d, m)
+// }
 
-func resourceSnippetExists(d *schema.ResourceData, m interface{}) (bool, error) {
-	return true, nil
-}
+// func resourceSnippetExists(d *schema.ResourceData, m interface{}) (bool, error) {
+// 	return true, nil
+// }
